5a57c740a338a925b05ef259a33ea51c9c48042b
kann1bal <mohamedzied.barketallah@esprit.tn>
2019-04-09 00:12:19 +0100

    temp 0.0

--- MAP.Presentation/Models/DocumentVM.cs
+++ MAP.Presentation/Models/DocumentVM.cs
@@ -23,14 +23,16 @@ namespace MAP.Presentation.Models
         public int DocumentId { get; set; }
         public DateTime DateDoc { get; set; }
         public string Name { get; set; }
+        [Display(Name = "Description")]
         public string Size { get; set; }
     
         public string ImageUrl { get; set; }
+        [Display(Name = "Type Document")]
         public TypeVm TypeVm { get; set; }
         public string Extension { get; set; }
 
         public string ProjectNames { get; set; }
-       
+        [Display(Name = "Project")]
         public int? ProjectId { get; set; }
         public IEnumerable<SelectListItem> Projects { get; set; }
 
